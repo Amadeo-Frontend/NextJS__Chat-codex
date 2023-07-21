93f42c58040cd0ee7ae7dc62592c30c356d40e87
Amadeo Bon <amadeosph@outlook.com>
2023-07-20 22:27:48 -0300

    first commit

---
+++ app/api/route.ts
@@ -0,0 +1,22 @@
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { Configuration, OpenAIApi } from "openai-edge";
+
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+    const {messages} = await req.json();
+
+    const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        stream:true,
+        messages: messages,
+    })
+    const stream = OpenAIStream(response);
+    return new StreamingTextResponse(stream);
+}
